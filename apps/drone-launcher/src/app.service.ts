018bc99304f4b1b5d9abf6b8ff105a3def58e9bd
Phelipe Rocha <phelipeafonso@gmail.com>
2023-05-24 19:08:36 -0300

    Bump drone-launcher version to v1.0.0

--- apps/drone-launcher/src/app.service.ts
+++ apps/drone-launcher/src/app.service.ts
@@ -13,7 +13,7 @@ export class AppService {
   ) {}
 
   getHello(): string {
-    return 'Drone Launcher Service v0.0.2';
+    return 'Drone Launcher Service v1.0.0';
   }
 
   enqueuLaunchProcess({
